5e3445a21326d5f4a565733963124ee3d0551143
Kajalk692001 <kumarikajal692001@gmail.com>
2025-06-11 12:56:59 +0530

    Add files via upload

---
+++ python.py
@@ -0,0 +1,30 @@
+X = 15
+if X > 10:
+    print("Above ten,")
+    if X > 20:
+        print("and also above 20!")
+    else:
+        print("but not above 20.")
+    
+    for i in range(5):
+        if i == 3:
+            pass # Placeholder for future code
+        else:
+            print(i)
+
+    i = 1
+    while  i < 6:
+        print(i)
+        if i == 3 :
+            break  
+        i += 1
+
+    i = 1
+    while i < 6 :
+        print(i)
+        i += 1
+
+    else :
+        print("i is no longer less than 6")
+
+        
